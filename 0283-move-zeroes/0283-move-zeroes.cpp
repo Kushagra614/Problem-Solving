f1f2ffeb1d9527e10901f88f4d4fb4ec0e3f90de
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2024-09-05 13:23:46 +0530

    Time: 7 ms (98.68%), Space: 22 MB (27.05%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        if(n== 0 || n== 1){
-            return;
-        }
-
-        int l = 0;
-        int r = 0;
-        int temp;
-        while(r<n)
+        int index = 0;
+        for(int i = 0;i<nums.size();i++)
         {
-            if(nums[r] == 0)
+            if(nums[i]!=0)
             {
-                r++;
-            }
-            else{
-                temp = nums[l];
-                nums[l] = nums[r];
-                nums[r] = temp;
-               l++;
-               r++;
-                
+                swap(nums[index], nums[i]);
+                index++;
             }
         }
     }
