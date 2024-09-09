c483174e32f8354e0484bd8c71374e2b4341f2c1
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2024-09-09 12:58:24 +0530

    Time: 97 ms (12.04%), Space: 96.2 MB (74.43%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    int findMax(const vector<int>& v) {
-        if (v.empty()) {
-            throw runtime_error("Cannot find maximum of an empty vector");
-        }
-        return *max_element(v.begin(), v.end());
-    }
-
     int maxProfit(vector<int>& prices) {
-        vector<int> v;
-        int diff = 0;
-        int j = 0;
-        for(int i = 1; i < prices.size(); i++) {
-            if(prices[j] < prices[i]) {
-                diff = prices[i] - prices[j];
-                v.push_back(diff);
-                continue;
+        if(prices.empty())
+        {
+            return 0;
+        }
+
+        int minP = prices[0];
+        int maxP = 0;
+
+        for(int i = 1; i< prices.size(); i++)
+        {
+            if(prices[i] < minP)
+            {
+                minP = prices[i];
             }
             else{
-                prices[j] = prices[i];
+                maxP = max(maxP, prices[i]-minP);
             }
-
-            
         }
-        
-       
-        if (v.empty()) {
-            return 0;  
-        }
-        return findMax(v);
+        return maxP;
     }
 };
