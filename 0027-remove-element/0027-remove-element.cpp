19531d9c78ebca1a4f437e5bc9829be59bda1ae6
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2024-10-07 13:19:26 +0530

    Time: 5 ms (27.94%), Space: 10.8 MB (36.91%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,19 +1,57 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int n = nums.size();
-        vector<int> nums1;
+        // int n = nums.size();
+        // vector<int> nums1;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != val) {
-                nums1.push_back(nums[i]);
-            }
-        }
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] != val) {
+        //         nums1.push_back(nums[i]);
+        //     }
+        // }
 
         
-        nums.clear();
-        nums = nums1;
+        // nums.clear();
+        // nums = nums1;
 
-        return nums.size();
+        // return nums.size();
+
+    //     int i = 0;
+    //     int n = nums.size();
+
+    //     for(int j = 0; j<n; j++)
+    //     {
+    //         if(nums[j]!=val)
+    //         {
+    //             nums[i] = nums[j];
+    //             i++;
+    //         }
+            
+    //     }
+    //     return i;
+    // }
+
+    int i = 0;
+    int j = nums.size()-1;
+    while(i<=j)
+    {
+        if(nums[j]==val)
+        {
+            j--;
+        }
+        else if(nums[i]==val && nums[j]!=val )
+        {
+            swap(nums[i],nums[j]);
+            j--;
+            i++;
+        }
+        else{
+            i++;
+        }
+        
     }
+    return i;
+    }
+
+
 };
