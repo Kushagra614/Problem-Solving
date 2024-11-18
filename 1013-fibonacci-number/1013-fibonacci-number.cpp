d00d01ff5483b7040c7f4a5c32945fc0aa396b50
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2024-11-18 11:51:08 +0530

    Time: 0 ms (100%), Space: 7.6 MB (11.55%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -13,8 +13,37 @@ public:
         int ans = fiboRec(n-1) + fiboRec(n-2);
         return ans;
     }
+
+    // USING DP - TOP DOWN (RECURSION + MEMEOISATION)
+    int fiboDP(int n, vector<int>&dp)
+    {
+        //base case
+        if(n==0||n==1)
+        {
+            return n;
+        }
+
+        //step 3: if ans already exists -> then return
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+
+        //step 2: store or return ans in dp
+        dp[n] = fiboDP(n-1, dp) + fiboDP(n-2, dp);
+        return dp[n];
+    }
+
+
     int fib(int n) {
-        int ans = fiboRec(n);
+        // int ans = fiboRec(n);
+        // return ans;
+
+        // Step 1 : Create DP array to store the ans
+        vector<int>dp(n+1,-1);
+
+
+        int ans = fiboDP(n,dp);
         return ans;
     }
 };
