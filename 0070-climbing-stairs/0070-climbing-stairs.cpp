01c5b75097dc359c061b8bf8927ce5a18180a6d0
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2024-11-20 16:56:35 +0530

    Time: 0 ms (100%), Space: 7.7 MB (9.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -56,6 +56,36 @@ public:
         return dp[n];
 
     }
+    // Now using Tabulation Method
+    int solveDPtabulation(int n,vector<int>&dp)
+    {
+         // base case
+        if (n == 0 || n == 1) {
+        return 1;
+    }
+
+        //step2: store inintal val in the array
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i<n+1 ; i++)
+        {
+
+        if(n>=0)
+        {
+        int one =  dp[i-1];
+        int two =  dp[i-2];
+
+
+        //step 2 : store the ans
+        dp[i] = one + two;
+        }
+
+        }
+
+        return dp[n];
+
+    }
 
     int climbStairs(int n) {
         
@@ -63,7 +93,8 @@ public:
         //step1: creating dp array
         vector<int>dp(n+1,INT_MAX);
         // int ans = solveRec(n);
-        int ans = solveDPmemo(n,dp);
+        // int ans = solveDPmemo(n,dp);
+        int ans = solveDPtabulation(n,dp);
         return ans;
 
     }
