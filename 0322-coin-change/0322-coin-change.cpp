7358344951f3ad4c109a97d62e25d1a884268e58
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2024-11-20 13:02:15 +0530

    Time: 23 ms (70.62%), Space: 16.4 MB (71.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -72,11 +72,63 @@ public:
         return dp[amount];
     }
 
+    //now going to solve by tabulation
+    /*
+    STEPS:
+    1) Create a dp array
+    2) put initial values in the dp array
+    3) now fill the dp array acc to the rec rel.
+
+    
+    */
+    int solveDPtabulation(vector<int>&coins, int amount,vector<int>&dp)
+    {
+        // base case
+        if(amount==0)
+        {
+            return 0;
+        }
+
+
+        //fill the val in dp array  
+        dp[0] = 0;
+        //for loop
+        
+        for(int k = 1; k<=amount; k++ )
+        {
+            int mini = INT_MAX;
+            for(int i = 0; i<coins.size();i++)
+        {
+           
+            int ans = 0;
+            
+            if(k - coins[i] >= 0)
+            {
+                
+                ans = dp[k-coins[i]];
+                if(ans !=INT_MAX)
+                {
+                int finalAns = 1 + ans;
+                mini= min(mini, finalAns);
+                }
+                
+            }
+             
+        }
+
+        dp[k] = mini;
+        }
+        return dp[amount];
+
+    }
+
 
     int coinChange(vector<int>& coins, int amount) {
         
-        vector<int>dp(amount+1,-1);
-        int ans = solveRec_Memo(coins,amount,dp);
+        // vector<int>dp(amount+1,-1);
+        vector<int>dp(amount+1,INT_MAX);
+        // int ans = solveRec_Memo(coins,amount,dp);
+        int ans = solveDPtabulation(coins,amount,dp);
         if(ans == INT_MAX)
         {
             return -1;
