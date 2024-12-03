a135453bc30cf88552b05d9c829c4a9de73a0829
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2024-12-03 13:10:22 +0530

    Time: 4 ms (80.7%), Space: 15.4 MB (39.46%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -18,6 +18,60 @@ public:
         }
         return count;
     }
+
+    //now solving using memo
+    int solveDPmemo(vector<int>&nums, int n, int s1,vector<vector<int>>&dp)
+    {
+        // base case
+        if(n==0) return s1 == 0? 1:0;
+
+        //check if ans exists
+        if(dp[n][s1] != -1)
+        {
+            return dp[n][s1];
+        }
+
+        //rec relation
+        int count = 0;
+        if(nums[n-1]<= s1)
+        {
+            count = solveDPmemo(nums,n-1,s1-nums[n-1],dp) + solveDPmemo(nums,n-1,s1,dp);
+        }
+        else
+        {
+            count = solveDPmemo(nums,n-1,s1,dp);
+        }
+        dp[n][s1] = count;
+        return dp[n][s1];
+    }
+
+    int solveDPtabulation(vector<int>&nums, int n, int s1,vector<vector<int>>&dp)
+    {
+        //base cases
+        for(int i = 0; i<= s1; i++)
+        {
+            dp[0][i] = (i == 0 ? 1:0);
+        }
+
+        //putting values in the dp arr
+        for(int i = 1; i<=n ; i++)
+        {
+            for(int j = 0; j<=s1; j++)
+            {
+                if(nums[i-1] <= j)
+                {
+                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] =  dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][s1];
+    }
+
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int tsum = 0;
@@ -30,7 +84,17 @@ public:
 
         int s1 = (tsum + target)/2;
 
-        return solveR(nums,n,s1);
+        // return solveR(nums,n,s1);
+        // If s1 is too large, return 0
+         if (s1 < 0 || s1 > 10000) {  // Adjust the limit as needed
+        return 0;
+        }
+
+        vector<vector<int>>dp(n+1, vector<int>(s1+1,-1));
+    
+
+        // return solveDPmemo(nums,n,s1,dp);
+        return solveDPtabulation(nums,n,s1,dp);
 
 
         
