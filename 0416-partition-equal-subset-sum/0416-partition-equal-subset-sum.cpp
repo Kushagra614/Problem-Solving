de33da943b7657cb5a1230f39175d7da8b73257d
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-01-30 14:59:08 +0530

    Time: 119 ms (58.71%), Space: 130.7 MB (12.53%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -26,11 +26,37 @@ public:
         return dp[n][k];
     }
 
-    // int DPtabu(vector<int>& nums, int n, int k,vector<vector<int>>&dp)
-    // {
-    //     //filling initial data
-        
-    // }
+    int DPtabu(vector<int>& nums, int n, int k,vector<vector<int>>&dp)
+    {
+        //filling initial data
+        for(int i = 0; i<= n; i++)
+        {
+            dp[i][0] = 1;
+        }
+        for(int j = 1; j<= k; j++)
+        {
+            dp[0][j] = 0;
+        }
+
+        //rec relation
+        for(int i = 1; i<= n; i++)
+        {
+            for (int j = 1; j<= k; j++)
+            {
+                if(nums[i-1] <= j)
+                {
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] =  dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][k];
+
+    }
     bool canPartition(vector<int>& nums) {
         int n= nums.size();
         
@@ -42,6 +68,8 @@ public:
         int k = sum/2;
         vector<vector<int>>dp(n+1, vector<int>(k+1,-1));
         if(sum&1) return false;
-        return  DPmemo(nums, n-1, k , dp); 
+        return DPtabu(nums, n-1, k , dp);
+        // return  DPmemo(nums, n-1, k , dp); 
+        
     }
 };
