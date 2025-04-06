c81a21aa4cf6e95823ef1b6d0ab15a8797c85e20
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-04-06 12:12:33 +0530

    Time: 0 ms (100%), Space: 30.2 MB (64.6%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -7,9 +7,34 @@ public:
             nums[i] = nums[i]*nums[i];
         }
 
-        //now sorting using two pointers
-        sort(nums.begin(), nums.end());
-        return nums;
+        // //now sorting using two pointers
+        // // sort(nums.begin(), nums.end());
+        // return nums;
+
+        int i = 0;
+        int j = nums.size()-1;
+        int pos = nums.size()-1;
+
+        vector<int>ans(nums.size());
+
+        while(i<=j)
+        {
+            if(nums[i]<nums[j])
+            {
+                ans[pos] = nums[j];
+                j--;
+                pos--;
+            }
+            else
+            {
+                ans[pos] = nums[i];
+                i++;
+                pos--;
+            }
+        }
+
+        return ans;
+        
 
 
     }
