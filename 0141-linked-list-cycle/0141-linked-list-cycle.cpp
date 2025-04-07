88b9ebe5fbeedfd22095daf5d589c99dfb41ad92
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-04-07 09:10:54 +0530

    Time: 12 ms (39.82%), Space: 11.7 MB (79.84%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,23 +10,47 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         
-        //USING HASHSET
-        unordered_set<ListNode*>vis;
+        //USING FAST AND SLOW POINTERS
 
-        ListNode* curr = head;
+        if(head == NULL) return false;
 
-        while(curr!= nullptr)
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast!= NULL && fast->next != NULL)
         {
-            if(vis.count(curr))
+            //they will before getting checked otherwise their starting at head will return true
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)
             {
                 return true;
             }
-
-            vis.insert(curr);
-            curr = curr->next;
         }
 
         return false;
+
+
+        //USING HASHSET
+        // unordered_set<ListNode*>vis;
+
+        // ListNode* curr = head;
+
+        // while(curr!= nullptr)
+        // {
+        //     if(vis.count(curr))
+        //     {
+        //         return true;
+        //     }
+
+        //     vis.insert(curr);
+        //     curr = curr->next;
+        // }
+
+        // return false;
+
+
 
         
     }
