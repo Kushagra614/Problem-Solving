20cd1d7f6ccf558cd3f68b20d000e3c37af7161c
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-05-18 17:01:35 +0530

    Time: 54 ms (42.7%), Space: 29 MB (74.32%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < nums.size() - 2; ++i) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue; 
 
             int j = i + 1;
             int k = nums.size() - 1;
@@ -15,7 +15,7 @@ public:
                 if (sum == 0) {
                     ans.push_back({nums[i], nums[j], nums[k]});
 
-                    // Skip duplicates for j and k
+                    
                     while (j < k && nums[j] == nums[j + 1]) ++j;
                     while (j < k && nums[k] == nums[k - 1]) --k;
 
