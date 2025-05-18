a0e5217758487eebd2a043ec6bc95be402a605e7
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-05-18 16:04:40 +0530

    Time: 0 ms (100%), Space: 63.1 MB (12.53%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,58 +1,29 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-
         int i = 0;
         int j = height.size()-1;
-        int maxW = INT_MIN;
+        int ans = 0;
+        int res = INT_MIN;
 
         while(i<j)
         {
-            int indi = j-i;
-            int h = min(height[i], height[j]);
-            int ans = indi*h;
-            maxW = max(maxW, ans);
-
-            if(height[i]<height[j])
+            if(height[i] < height[j])
             {
+                ans = height[i] * (j-i);
+                res = max(res, ans);
                 i++;
+
             }
+
             else
             {
+                ans = height[j] * (j-i);
+                res = max(res, ans);
                 j--;
             }
-        }
-        return maxW;
 
-        // int maxW = INT_MIN;
-        // for(int i = 0; i<height.size(); i++)
-        // {
-        //     int j = i+1;
-            
-        //     while(j<height.size())
-        //     {
-        //         int indi = j-i;
-        //         if(height[i]>height[j])
-        //         {
-                   
-        //             int ans = indi* height[j];
-        //             if(ans>maxW)
-        //             {
-        //                 maxW = ans;
-        //             }
-                    
-        //         }
-        //         else
-        //         {
-        //             int ans = indi* height[i];
-        //             if(ans>maxW)
-        //             {
-        //                 maxW = ans;
-        //             }
-        //         }
-        //         j++;
-        //     }
-        // }
-        // return maxW;
+        }
+        return res;
     }
 };
