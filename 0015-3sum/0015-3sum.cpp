c3b9daa00784ee0f46936c410c97104a149e2530
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-05-18 16:49:03 +0530

    Time: 41 ms (90.34%), Space: 29 MB (90.52%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,84 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
-
-        vector<vector<int>>ans;
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i<nums.size(); i++)
-        {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int j = i+1;
-            int k = nums.size()-1;
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates for i
 
-            while(j<k)
-            {
-                if(nums[i] + nums[j] + nums[k] == 0)
-                {
-                vector<int>trip = {nums[i], nums[j], nums[k]};
-                ans.push_back(trip);
+            int j = i + 1;
+            int k = nums.size() - 1;
 
-                while (j < k && nums[j] == nums[j + 1]) j++;
-                    
-                while (j < k && nums[k] == nums[k - 1]) k--;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[j], nums[k]});
 
-                    j++;
-                    k--;
+                    // Skip duplicates for j and k
+                    while (j < k && nums[j] == nums[j + 1]) ++j;
+                    while (j < k && nums[k] == nums[k - 1]) --k;
 
-                }
-                else if(nums[i]+ nums[j]+ nums[k] < 0)
-                {
-                    j++;
-                }
-                else
-                {
-                    k--;
+                    ++j;
+                    --k;
+                } else if (sum < 0) {
+                    ++j;
+                } else {
+                    --k;
                 }
             }
         }
-
-return ans;
-
-
-
-
-        // vector<vector<int>>ans;
-        // //brute force
-
-        // for(int i = 0; i<nums.size(); i++)
-        // {
-        //     for(int j = i+1; j<nums.size(); j++)
-        //     {
-        //         for(int k = j+1; k<nums.size(); k++)
-        //         {
-        //             if(nums[i] + nums[j] + nums[k] == 0)
-        //             {
-        //                 vector<int>trip = {nums[i], nums[j], nums[k]};
-        //                 sort(trip.begin(), trip.end());
-                        
-        //                 bool exists = false;
-        //                 for(auto vec: ans){
-        //                     if(vec == trip)
-        //                     {
-        //                         exists = true;
-        //                         break;
-        //                     }
-        //                 }
-
-        //                 if(!exists)
-        //                 {
-        //                     ans.push_back(trip);
-        //                 }
-
-        //             }
-        //         }
-        //     }
-        // }
-        // return ans;
-
-
-
-
+        return ans;
     }
 };
