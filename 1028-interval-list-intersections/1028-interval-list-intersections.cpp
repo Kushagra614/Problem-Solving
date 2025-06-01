12fca25333400c15d293790f1f86394ad784dfb7
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-06-01 08:50:00 +0530

    Time: 0 ms (100%), Space: 22.8 MB (98.63%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.cpp
+++ 1028-interval-list-intersections/1028-interval-list-intersections.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList,
+                                             vector<vector<int>>& secondList) {
+        int n1 = firstList.size();
+        int n2 = secondList.size();
         vector<vector<int>>ans;
-        int i = 0, j= 0;
+        int i = 0;
+        int j = 0;
 
-        while(i< firstList.size() && j< secondList.size())
+        while (i < n1 && j < n2) 
         {
-            int s = max(firstList[i][0], secondList[j][0]);
-            int e = min(firstList[i][1], secondList[j][1]);
+           int s = max(firstList[i][0], secondList[j][0]);
+           int e = min(firstList[i][1], secondList[j][1]);
 
-            if(s<= e)
-            {
-                ans.push_back({s,e});
-            }
+           if(s<=e)
+           {
+            ans.push_back({s,e});
+           }
 
-            if(firstList[i][1] < secondList[j][1])
-            {
-                i++;
-            }
-            else
-            {
-                j++;
-            }
-            
+           //moving pointer
+           if(firstList[i][1] < secondList[j][1])
+           {
+            i++;
+           }
+           else
+           {
+            j++;
+           }
         }
         return ans;
-        
     }
 };
