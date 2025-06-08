45613b80c9c547885fa43eaf349c872c226f568d
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-06-08 15:17:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,54 +1,114 @@
 class Solution {
   public:
   
-    void dfs(int node, vector<int>&vis, vector<vector<int>>&adj, stack<int>&st)
-    {
-        vis[node] = 1;
-        for(auto i : adj[node])
-        {
-            if(!vis[i])
-            {
-                dfs(i, vis, adj, st);
-            }
-        }
+    // void dfs(int node, vector<int>&vis, vector<vector<int>>&adj, stack<int>&st)
+    // {
+    //     vis[node] = 1;
+    //     for(auto i : adj[node])
+    //     {
+    //         if(!vis[i])
+    //         {
+    //             dfs(i, vis, adj, st);
+    //         }
+    //     }
         
-        st.push(node); 
+    //     st.push(node); 
         
-    }
+    // }
+    // vector<int> topoSort(int V, vector<vector<int>>& edges) {
+    //     vector<int>vis(V, 0);
+    //     stack<int>st;
+    //     vector<vector<int>>adj(V);
+    //     vector<int>ans;
+        
+    //     //changing edges into adj
+    //     for( auto e : edges)
+    //     {
+    //         int u = e[0];
+    //         int v = e[1];
+    //         adj[u].push_back(v);
+    //     }
+        
+    //     for(int j = 0; j < V; j++)
+    //     {
+    //         if(!vis[j])
+    //         {
+    //             dfs(j, vis, adj, st);
+    //         }
+            
+    //     }
+        
+        
+    //     while(!st.empty())
+    //     {
+    //         int s = st.top();
+    //         st.pop();
+    //         ans.push_back(s);
+    //     }
+        
+    //     return ans;
+        
+        // -X-X-X-X-X-X--X-X-X-X-X-X-X-X-X--XX--X-X-X-X-X-X-X-X-X-X-X-X-X-X-X
+        //  NOW USING BFS ( KAHNS ALGO)
+
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        vector<int>vis(V, 0);
-        stack<int>st;
-        vector<vector<int>>adj(V);
-        vector<int>ans;
-        
-        //changing edges into adj
-        for( auto e : edges)
+    
+    vector<vector<int>>adj(V);
+    vector<int>indeg(V);
+    queue<int>q;
+    
+        //edges to adj
+        for(auto e : edges)
         {
             int u = e[0];
             int v = e[1];
             adj[u].push_back(v);
-        }
-        
-        for(int j = 0; j < V; j++)
-        {
-            if(!vis[j])
-            {
-                dfs(j, vis, adj, st);
-            }
             
         }
         
-        
-        while(!st.empty())
+        // find all the indegree
+        for(int i = 0; i< V; i++)
         {
-            int s = st.top();
-            st.pop();
-            ans.push_back(s);
+            for(auto j : adj[i])
+            {
+                indeg[j]++;
+            }
+        }
+        
+        
+        // 0 indeg to queue
+        for(int i = 0; i< V; i++)
+        {
+            if(indeg[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+        
+        
+        //now do bfs
+        vector<int>ans;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto nbr : adj[node])
+            {
+                indeg[nbr]--;
+                if(indeg[nbr] == 0)
+                {
+                    q.push(nbr);
+                }
+            }
         }
         
         return ans;
         
         
+        
+    
         
         
     }
