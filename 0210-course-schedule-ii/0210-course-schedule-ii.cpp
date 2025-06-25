44eea2a5968eaaa10e53b5661d21c6239a108598
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-06-25 23:33:38 +0530

    Time: 3 ms (73.29%), Space: 18.6 MB (44.73%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>indeg(numCourses);
+        queue<int>q;
+        vector<int>ans;
+
+         //creating adj list
+        for(auto e : prerequisites)
+        {
+            int u = e[0];
+            int v = e[1];
+            adj[u].push_back(v);
+        }
+
+        //finding all the indeg
+        for(int i = 0; i<numCourses; i++)
+        {
+            for(auto j : adj[i])
+            {
+                indeg[j]++;
+            }
+        }
+
+        //all the nodes of indeg 0 to be pushed to queue
+        for(int i = 0; i<numCourses; i++)
+        {
+            if(indeg[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+
+        //now doing bfs
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto i : adj[node])
+            {
+                indeg[i]--;
+                if(indeg[i] == 0)
+                {
+                    q.push(i);
+                }
+            }
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans.size() == numCourses ? ans : vector<int>();
+    }
+};
