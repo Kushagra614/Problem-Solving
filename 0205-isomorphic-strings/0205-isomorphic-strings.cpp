6c2a6283cb94efac9eb05fd0b4fd58408b6ae8c0
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-06-29 06:39:42 +0530

    Time: 0 ms (100%), Space: 9.3 MB (56.81%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,40 +1,80 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+
+        //BRUTE FORCE
+
+        // if (s.length() != t.length()) {
+        //     return false;
+        // }
+        
+        // unordered_map<char, char> mapping;
+        // string transformed = s;
+        
+        // // Try to build mapping from s to t
+        // for (int i = 0; i < s.length(); i++) {
+        //     char char_s = s[i];
+        //     char char_t = t[i];
+            
+        //     if (mapping.find(char_s) != mapping.end()) {
+        //         // Check if existing mapping conflicts
+        //         if (mapping[char_s] != char_t) {
+        //             return false;
+        //         }
+        //     } else {
+        //         // Check if char_t is already mapped to another character
+        //         for (auto& pair : mapping) {
+        //             if (pair.second == char_t) {
+        //                 return false;
+        //             }
+        //         }
+        //         mapping[char_s] = char_t;
+        //     }
+        // }
+        
+        // // Replace characters in s using the mapping
+        // for (int i = 0; i < transformed.length(); i++) {
+        //     transformed[i] = mapping[transformed[i]];
+        // }
+        
+        // // Check if transformed string equals t
+        // return transformed == t;
+
+        // ------X------------X----------X------------X-----------X-----------X------------X-----------X
+
+        // OPTIMAL APPRAOCH
+
         if (s.length() != t.length()) {
             return false;
         }
-        
-        unordered_map<char, char> mapping;
-        string transformed = s;
-        
-        // Try to build mapping from s to t
-        for (int i = 0; i < s.length(); i++) {
+
+        unordered_map<char, char>mp;
+        unordered_set<char>st;
+
+        for(int i = 0; i< s.size(); i++)
+        {
             char char_s = s[i];
             char char_t = t[i];
-            
-            if (mapping.find(char_s) != mapping.end()) {
-                // Check if existing mapping conflicts
-                if (mapping[char_s] != char_t) {
+
+            if(mp.find(char_s) != mp.end())
+            {
+                if(mp[char_s] != char_t)
+                {
                     return false;
                 }
-            } else {
-                // Check if char_t is already mapped to another character
-                for (auto& pair : mapping) {
-                    if (pair.second == char_t) {
-                        return false;
-                    }
-                }
-                mapping[char_s] = char_t;
             }
+            else
+            {
+                if(st.count(char_t)) return false;
+            }
+
+            //create new mapping
+            mp[char_s] = char_t;
+            st.insert(char_t);
         }
-        
-        // Replace characters in s using the mapping
-        for (int i = 0; i < transformed.length(); i++) {
-            transformed[i] = mapping[transformed[i]];
-        }
-        
-        // Check if transformed string equals t
-        return transformed == t;
+
+        return true;
+
+
     }
 };
