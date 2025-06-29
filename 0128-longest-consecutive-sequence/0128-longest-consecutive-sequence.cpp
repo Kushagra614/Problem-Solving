f6de04cdc5e54144d25a3504291124d0c592f37d
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-06-29 16:30:41 +0530

    Time: 91 ms (33.24%), Space: 88.9 MB (49.68%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,30 +1,60 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if (nums.size() == 0) return 0;
+    //     if (nums.size() == 0) return 0;
 
-        set<int> st;
-        vector<int> ans;
+    //     set<int> st;
+    //     vector<int> ans;
 
-        for(auto i : nums) st.insert(i);
-        for(auto i : st) ans.push_back(i);
+    //     for(auto i : nums) st.insert(i);
+    //     for(auto i : st) ans.push_back(i);
 
+    //     int maxLen = 1;
+    //     int count = 1;
+
+    //     for (int i = 0; i < ans.size()-1; i++) {
+
+    //         if (abs(ans[i] - ans[i + 1]) == 1)
+    //         {
+    //             count++;
+    //         }
+    //         else
+    //         {
+    //             count = 1;
+    //         }
+
+    //         maxLen = max(count, maxLen);
+    //     }
+    //     return maxLen;
+
+    // -------x------X------X-----------X-----------X---------X----------X
+
+        if(nums.size() == 0) return 0;
         int maxLen = 1;
-        int count = 1;
+        unordered_set<int>st;
 
-        for (int i = 0; i < ans.size()-1; i++) {
-
-            if (abs(ans[i] - ans[i + 1]) == 1)
-            {
-                count++;
-            }
-            else
-            {
-                count = 1;
-            }
-
-            maxLen = max(count, maxLen);
+        for(auto i: nums)
+        {
+            st.insert(i);
         }
+
+
+        for(auto it : st)
+        {
+            if(st.find(it - 1) == st.end())
+            {
+                int cnt = 1;
+                int x = it;
+                while(st.find(x+1) != st.end())
+                {
+                    x++;
+                    cnt++;
+                }
+
+                maxLen = max(maxLen, cnt);
+            }
+        }
+
         return maxLen;
     }
 };
