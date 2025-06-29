4ff568d51b1482cca54d8cb3138c392dd5a48dca
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-06-29 14:49:27 +0530

    Time: 148 ms (11.85%), Space: 95.5 MB (5.29%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -3,11 +3,12 @@ public:
     int longestConsecutive(vector<int>& nums) {
         if (nums.size() == 0) return 0;
         if (nums.size() == 0) return 0;
-        set<int> st(nums.begin(), nums.end());
+
+        set<int> st;
         vector<int> ans;
-        for (auto i : st) {
-            ans.push_back(i);
-        }
+
+        for(auto i: nums) st.insert(i);
+        for(auto i : st) ans.push_back(i);
 
         int maxLen = 1;
         int count = 1;
