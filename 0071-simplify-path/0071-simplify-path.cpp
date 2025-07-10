0afb13eff9ff7b01c7525e9ae930fce5cb21aae5
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-07-11 02:09:30 +0530

    Time: 0 ms (100%), Space: 15.3 MB (9.97%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,99 @@
+class Solution {
+public:
+//     string simplifyPath(string path) {
+
+//         // create a stack
+//         stack<char> st;
+//         if(path.empty()) return "/";
+//         if(path.size() > 0) st.push('/');
+        
+
+//         for (int i = 1; i<path.size();) 
+//         {
+//             // condtions
+//             if (path[i] == '/') 
+//             {
+//                 if (!st.empty() && st.top() != '/') 
+//                 {
+//                     st.push('/');
+                    
+//                 }
+//                 i++;
+//             }
+
+//             else if (path[i] == '.') 
+//             {
+//                 if (i+1 < path.size() && path[i+1] == '.' && 
+//                    (i+2 >= path.size() || path[i+2] == '/'))
+//                 {
+//                     if(st.size() > 1)
+//                     {
+//                         st.pop();
+//                         while(!st.empty() && st.top() != '/')
+//                         {
+//                             st.pop();
+//                         }
+//                     }
+
+//                     i += 2;
+                    
+//                 }
+//                 else //('.')
+//                 {
+//                     i++;
+//                 }
+//             }
+
+//             else
+//             {
+//                 st.push(path[i]);
+//                 i++;
+//             }
+
+//         }
+
+//         //checking if / at the end
+//         if(st.size() > 1 && st.top() == '/') st.pop();
+
+//         string ans = "";
+
+//         while(!st.empty())
+//         {
+//             auto ch = st.top();
+//             st.pop();
+//             ans+=ch;
+//         }
+
+//         reverse(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
+
+
+string simplifyPath(string path) {
+    stack<string> st;
+    stringstream ss(path);
+    string token;
+    
+    while (getline(ss, token, '/')) {
+        if (token == "" || token == ".") continue;
+        if (token == "..") {
+            if (!st.empty()) st.pop();
+        } else {
+            st.push(token);
+        }
+    }
+    
+        string ans = "";
+
+        while(!st.empty())
+        {
+            auto ch = st.top();
+            st.pop();
+            ans= "/" + ch + ans;
+        }
+
+        // reverse(ans.begin(), ans.end());
+        return ans.empty() ? "/" : ans;
+}
+};
