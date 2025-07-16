1a3c4520584c84bd011c8337d9a8b2ece9e656e1
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-07-16 18:56:13 +0530

    Time: 17 ms (64.66%), Space: 29.9 MB (14.27%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -1,8 +1,42 @@
 class Solution {
 public:
     string removeStars(string s) {
-        stack<char>st;
-        stack<char>rev;
+    //     stack<char>st;
+    //     stack<char>rev;
+    //     string res = "";
+
+    //     for(int i = 0; i<s.size(); i++)
+    //     {
+    //         char ch = s[i];
+    //         if(ch == '*')
+    //         {
+    //             st.pop();
+    //         }
+    //         else if (ch != '*')
+    //         {
+    //             st.push(ch);
+    //         }
+            
+    //     }
+
+    //     while(!st.empty())
+    //     {
+    //         int node = st.top();
+    //         st.pop();
+    //         rev.push(node);
+    //     }
+
+    //     while(!rev.empty())
+    //     {
+    //         res += rev.top();
+    //         rev.pop();
+    //     }
+
+    //     return res;
+    // }
+    // -X------------X------------X----------X-----------X----------X---------X--------------X----------------X
+
+        deque<char>st;
         string res = "";
 
         for(int i = 0; i<s.size(); i++)
@@ -10,27 +44,27 @@ public:
             char ch = s[i];
             if(ch == '*')
             {
-                st.pop();
+                st.pop_back();
             }
             else if (ch != '*')
             {
-                st.push(ch);
+                st.push_back(ch);
             }
             
         }
 
         while(!st.empty())
         {
-            int node = st.top();
-            st.pop();
-            rev.push(node);
+            char node = st.front();
+            st.pop_front();
+            res += node;
         }
 
-        while(!rev.empty())
-        {
-            res += rev.top();
-            rev.pop();
-        }
+        // while(!rev.empty())
+        // {
+        //     res += rev.top();
+        //     rev.pop();
+        // }
 
         return res;
     }
