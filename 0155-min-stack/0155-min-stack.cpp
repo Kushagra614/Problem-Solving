567b1cc0a73b123c08b2471f732861fb82f1ace9
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-07-31 16:46:46 +0530

    Time: 3 ms (62.01%), Space: 23.2 MB (75.21%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,76 +1,79 @@
-// class MinStack {
-// public:
-//     stack<long long> st;
-//     long long mini;
-
-//     MinStack() {mini = INT_MAX;}
-
-//     void push(int val) {
-//         // 1st ele
-//         long long value = val;
-//         if (st.empty()) {
-//             st.push(value);
-//             mini = value;
-//         }
-
-//         else {
-//             if (value < mini) {
-//                 st.push(2 * value - mini);
-//                 mini = value;
-//             }
-
-//             else {
-//                 st.push(value);
-//             }
-//         }
-//     }
-
-//     void pop() {
-//         if (st.empty()) return;
-
-//             long long curr = st.top();
-//             st.pop();
-
-//             if(curr < mini) {
-//                 mini = 2 * mini - curr;
-//             }
-
-//         }
-
-//     int top() {
-//         if (st.empty())
-//             return -1;
-
-//         long long curr = st.top();
-//         if (curr >= mini) {
-//             return curr;
-//         } else {
-//             return mini;
-//         }
-//     }
-
-//     int getMin() { return (int)mini; }
-// };
-
-
 class MinStack {
-    stack<pair<int, int>> st;
-    int mini = INT_MAX;
+     public:
+         stack<long long> st;
+         long long mini;
 
-public:
-    void push(int val) {
-        if (st.empty()) {
-            mini = val;
-        } else {
-            mini = min(st.top().second, val);
-        }
-        st.push({val, mini});
-    }
+         MinStack() {mini = INT_MAX;}
 
-    void pop() { st.pop(); }
-    int top() { return st.top().first; }
-    int getMin() { return st.top().second; }
-};
+         void push(int val) {
+             // 1st ele
+             long long value = val;
+             if (st.empty()) {
+                 st.push(value);
+                 mini = value;
+             }
+
+             else {
+                 if (value < mini) {
+                     st.push(2 * value - mini);
+                     mini = value;
+                 }
+
+                 else {
+                     st.push(value);
+                 }
+             }
+         }
+
+         void pop() {
+             if (st.empty()) return;
+
+                 long long curr = st.top();
+                 st.pop();
+
+                 if(curr < mini) {
+                     mini = 2 * mini - curr;
+                 }
+
+             }
+
+         int top() {
+             if (st.empty())
+                 return -1;
+
+             long long curr = st.top();
+             if (curr >= mini) {
+                 return curr;
+             } else {
+                 return mini;
+             }
+             
+         }
+
+         int getMin() { return (int)mini; }
+     };
+
+
+
+        
+
+    
+
+        
+
+    
+
+        
+    
+    
+
+
+
+    
+    
+        
+    
+
 
 /**
  * Your MinStack object will be instantiated and called as such:
