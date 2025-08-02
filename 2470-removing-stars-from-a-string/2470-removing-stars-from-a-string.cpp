20ca68f5020cd2d955dec03152644b88060fc021
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-08-02 21:57:06 +0530

    Time: 24 ms (29.79%), Space: 29.9 MB (24.12%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -1,70 +1,31 @@
 class Solution {
 public:
     string removeStars(string s) {
-    //     stack<char>st;
-    //     stack<char>rev;
-    //     string res = "";
-
-    //     for(int i = 0; i<s.size(); i++)
-    //     {
-    //         char ch = s[i];
-    //         if(ch == '*')
-    //         {
-    //             st.pop();
-    //         }
-    //         else if (ch != '*')
-    //         {
-    //             st.push(ch);
-    //         }
-            
-    //     }
-
-    //     while(!st.empty())
-    //     {
-    //         int node = st.top();
-    //         st.pop();
-    //         rev.push(node);
-    //     }
-
-    //     while(!rev.empty())
-    //     {
-    //         res += rev.top();
-    //         rev.pop();
-    //     }
-
-    //     return res;
-    // }
-    // -X------------X------------X----------X-----------X----------X---------X--------------X----------------X
-
-        deque<char>st;
-        string res = "";
-
-        for(int i = 0; i<s.size(); i++)
+        //using deque
+        deque<char>dq;
+        for(int i = 0; i < s.size(); i++)
         {
             char ch = s[i];
             if(ch == '*')
             {
-                st.pop_back();
+                if(!dq.empty())
+                {
+                    dq.pop_back();
+                }
             }
-            else if (ch != '*')
+            else
             {
-                st.push_back(ch);
+                dq.push_back(ch);
             }
-            
         }
 
-        while(!st.empty())
+        string res = "";
+        while(!dq.empty())
         {
-            char node = st.front();
-            st.pop_front();
-            res += node;
+            char sh = dq.front();
+            dq.pop_front();
+            res += sh;
         }
-
-        // while(!rev.empty())
-        // {
-        //     res += rev.top();
-        //     rev.pop();
-        // }
 
         return res;
     }
