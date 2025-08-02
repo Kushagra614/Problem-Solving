b3695145bfd63137ffd015caccc389397806a898
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-08-02 16:58:23 +0530

    Time: 0 ms (100%), Space: 19.1 MB (7.48%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<string> st;
-        int i = 0;
-        int cal = 0;
-        
-        while (i < tokens.size()) {
-            if (st.empty()) {
-                string n = tokens[i];
-                st.push(n);
-                i++;
-            }
-            else {
-                if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "/" || tokens[i] == "*") {
-                    string t1 = st.top();
-                    st.pop();
-                    int v1 = stoi(t1);
-
-                    string t2 = st.top();
-                    st.pop();
-                    int v2 = stoi(t2);
-
-                    if (tokens[i] == "+") {
-                        cal = v2 + v1;
-                    } else if (tokens[i] == "-") {
-                        cal = v2 - v1;
-                    } else if (tokens[i] == "/") {
-                        cal = v2 / v1;
-                    } else if (tokens[i] == "*") {
-                        cal = v2 * v1;
+        stack<string>st;
+        int ans = 0;
+        for(auto i : tokens)
+        {
+            
+            if(isdigit(i[0]) || i.size() > 1) st.push(i);
+            else
+            {
+                if(st.size() >= 2)
+                {
+                    if(i == "+")
+                    {
+                        string v1 = st.top(); st.pop();
+                        string v2 = st.top(); st.pop();
+                        ans = stoi(v1) + stoi(v2);
                     }
-
-                    st.push(to_string(cal));
+                    else if(i == "-")
+                    {
+                        string v1 = st.top(); st.pop();
+                        string v2 = st.top(); st.pop();
+                        ans = stoi(v2) - stoi(v1);
+                    }
+                    else if(i == "*")
+                    {
+                        string v1 = st.top(); st.pop();
+                        string v2 = st.top(); st.pop();
+                        ans = stoi(v1) * stoi(v2);
+                    }
+                    else if(i == "/")
+                    {
+                        string v1 = st.top(); st.pop();
+                        string v2 = st.top(); st.pop();
+                        ans = stoi(v2) / stoi(v1);
+                    }
+                    st.push(to_string(ans));
                 }
-                else {
-                    st.push(tokens[i]);
-                }
-                i++;
             }
         }
-        
         return stoi(st.top());
     }
 };
