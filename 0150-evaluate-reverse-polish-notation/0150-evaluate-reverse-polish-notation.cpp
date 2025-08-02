b9f7db0f14cd9d83d9909f915509984c230e272a
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-08-02 17:01:13 +0530

    Time: 0 ms (100%), Space: 19.1 MB (7.48%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -11,30 +11,12 @@ public:
             {
                 if(st.size() >= 2)
                 {
-                    if(i == "+")
-                    {
-                        string v1 = st.top(); st.pop();
-                        string v2 = st.top(); st.pop();
-                        ans = stoi(v1) + stoi(v2);
-                    }
-                    else if(i == "-")
-                    {
-                        string v1 = st.top(); st.pop();
-                        string v2 = st.top(); st.pop();
-                        ans = stoi(v2) - stoi(v1);
-                    }
-                    else if(i == "*")
-                    {
-                        string v1 = st.top(); st.pop();
-                        string v2 = st.top(); st.pop();
-                        ans = stoi(v1) * stoi(v2);
-                    }
-                    else if(i == "/")
-                    {
-                        string v1 = st.top(); st.pop();
-                        string v2 = st.top(); st.pop();
-                        ans = stoi(v2) / stoi(v1);
-                    }
+                    string v1 = st.top(); st.pop();
+                    string v2 = st.top(); st.pop();
+                    if(i == "+") ans = stoi(v1) + stoi(v2);
+                    else if(i == "-") ans = stoi(v2) - stoi(v1);
+                    else if(i == "*") ans = stoi(v1) * stoi(v2);
+                    else if(i == "/") ans = stoi(v2) / stoi(v1);
                     st.push(to_string(ans));
                 }
             }
