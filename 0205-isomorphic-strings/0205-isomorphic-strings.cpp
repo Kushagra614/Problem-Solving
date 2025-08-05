21639a3ea19e2960cb03d3eef7a0f5e28ba3e26e
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-08-05 12:53:28 +0530

    Time: 0 ms (100%), Space: 9.5 MB (29.92%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,76 +4,71 @@ public:
 
         //BRUTE FORCE
 
-        // if (s.length() != t.length()) {
-        //     return false;
-        // }
+        if (s.length() != t.length()) {
+            return false;
+        }
         
-        // unordered_map<char, char> mapping;
-        // string transformed = s;
+        unordered_map<char, char> mapping;
+        string transformed = s;
         
-        // // Try to build mapping from s to t
-        // for (int i = 0; i < s.length(); i++) {
-        //     char char_s = s[i];
-        //     char char_t = t[i];
+        // Try to build mapping from s to t
+        for (int i = 0; i < s.length(); i++) {
+            char char_s = s[i];
+            char char_t = t[i];
             
-        //     if (mapping.find(char_s) != mapping.end()) {
-        //         // Check if existing mapping conflicts
-        //         if (mapping[char_s] != char_t) {
-        //             return false;
-        //         }
-        //     } else {
-        //         // Check if char_t is already mapped to another character
-        //         for (auto& pair : mapping) {
-        //             if (pair.second == char_t) {
-        //                 return false;
-        //             }
-        //         }
-        //         mapping[char_s] = char_t;
-        //     }
-        // }
+            if (mapping.find(char_s) != mapping.end()) {
+                // Check if existing mapping conflicts
+                if (mapping[char_s] != char_t) {
+                    return false;
+                }
+            } else {
+                // Check if char_t is already mapped to another character
+                for (auto& pair : mapping) {
+                    if (pair.second == char_t) {
+                        return false;
+                    }
+                }
+                mapping[char_s] = char_t;
+            }
+
+        }
         
-        // // Replace characters in s using the mapping
-        // for (int i = 0; i < transformed.length(); i++) {
-        //     transformed[i] = mapping[transformed[i]];
-        // }
-        
-        // // Check if transformed string equals t
-        // return transformed == t;
+       return true;
 
         // ------X------------X----------X------------X-----------X-----------X------------X-----------X
 
         // OPTIMAL APPRAOCH
 
-        if (s.length() != t.length()) {
-            return false;
-        }
+        // if (s.length() != t.length()) {
+        //     return false;
+        // }
 
-        unordered_map<char, char>mp;
-        unordered_set<char>st;
+        // unordered_map<char, char>mp;
+        // unordered_set<char>st;
 
-        for(int i = 0; i< s.size(); i++)
-        {
-            char char_s = s[i];
-            char char_t = t[i];
+        // for(int i = 0; i< s.size(); i++)
+        // {
+        //     char char_s = s[i];
+        //     char char_t = t[i];
 
-            if(mp.find(char_s) != mp.end())
-            {
-                if(mp[char_s] != char_t)
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                if(st.count(char_t)) return false;
-            }
+        //     if(mp.find(char_s) != mp.end())
+        //     {
+        //         if(mp[char_s] != char_t)
+        //         {
+        //             return false;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         if(st.count(char_t)) return false;
+        //     }
 
-            //create new mapping
-            mp[char_s] = char_t;
-            st.insert(char_t);
-        }
+        //     //create new mapping
+        //     mp[char_s] = char_t;
+        //     st.insert(char_t);
+        // }
 
-        return true;
+        // return true;
 
 
     }
