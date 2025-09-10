c636c110dd733f7ba041e512dd5f613382b5ba6d
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-09-10 10:15:10 +0530

    Time: 0 ms (100%), Space: 17.7 MB (57.09%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxProduct = INT_MIN;
+
+        // ------------BRUTE FORCE-------------------------------
+        // int maxProduct = INT_MIN;
+        // int n = nums.size();
+
+        // for (int i = 0; i < n; i++) {
+        //     int currentProduct = 1;
+        //     for (int j = i; j < n; j++) {
+        //         currentProduct *= nums[j];
+        //         if (currentProduct > maxProduct) {
+        //             maxProduct = currentProduct;
+        //         }
+        //     }
+        // }
+
+        // return maxProduct;
+
+
+        // ------------------OPTIMIZED APPROACH----------------------
+        int maxPro = INT_MIN;
+        int pre = 1, suff = 1;
         int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            int currentProduct = 1;
-            for (int j = i; j < n; j++) {
-                currentProduct *= nums[j];
-                if (currentProduct > maxProduct) {
-                    maxProduct = currentProduct;
-                }
-            }
+        for(int i = 0; i < n; i++)
+        {
+            if(pre == 0) pre = 1;
+            if(suff == 0) suff = 1;
+            pre*=nums[i];
+            suff*=nums[n-i-1];
+            maxPro = max(maxPro, max(pre,suff));
         }
-
-        return maxProduct;
+         return maxPro;
     }
 };
