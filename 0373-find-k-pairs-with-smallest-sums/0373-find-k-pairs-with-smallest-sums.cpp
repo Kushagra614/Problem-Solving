533b498f827bccf4c5894af542b2faedac4007b7
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-09-14 16:31:36 +0530

    Time: 84 ms (42.25%), Space: 141.6 MB (55.01%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,81 +1,46 @@
 class Solution {
 public:
-typedef pair<int, pair<int,int>> P;
+    typedef pair<int, pair<int,int>> P;
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-    //     //brute force
-    //    priority_queue<
-    //     pair<int, pair<int, int> >,
-    //     vector<pair<int, pair<int, int> > >,
-    //     greater<pair<int, pair<int, int> > >
-    // > pq;
+        //min heap
+        priority_queue<P, vector<P>, greater<P>>pq;
 
-    //     for(int i = 0; i < nums1.size(); i++)
-    //     {
-    //         for(int j = 0; j < nums2.size(); j++)
-    //         {
-    //             int sum = nums1[i] + nums2[j];
-    //             pq.push({sum, {nums1[i], nums2[j]}});
-    //         }
-    //     }
+        int n = nums1.size();
+        int m = nums2.size();
+    
+        //pushing the 1st 2 ele
+        pq.push({nums1[0] + nums2[0], {0,0}});
+        //creating a set
+        set<pair<int,int>>st;
 
-        
+        st.insert({0,0});
+        vector<vector<int>>ans;
 
-    //     // now pop till k
-    //     vector<vector<int>>ans;
-    //     while(k>0)
-    //     {
-    //         auto topp = pq.top(); pq.pop();
-    //         auto res = topp.second;
-    //         ans.push_back({res.first, res.second});
-    //         k--;
-    //         if(k == 0) break;
-    //     }
-
-    //     return ans;
-    //     }
-
-    // --------X-----------X-------------OPTIMIZED-------------X--------------X----------
-    priority_queue<P, vector<P>, greater<P>>pq;
-
-    int n = nums1.size();
-    int m = nums2.size();
-
-    set<pair<int,int>>vis;
-
-    int sum = nums1[0]+nums2[0];
-    pq.push({sum, {0,0}});
-    vis.insert({0,0});
-
-
-    vector<vector<int>>res;
-
-    while(k>0 && !pq.empty())
-    {
-        auto topp = pq.top();
-        pq.pop();
-
-        int i = topp.second.first;
-        int j = topp.second.second;
-
-        res.push_back({nums1[i], nums2[j]});
-        k--;
-
-        // now we will check 
-        // (i+1,j) -> if in bounds
-        if(i+1 < n && vis.find({i+1,j})== vis.end())
+        while(k > 0 && pq.size() > 0)
         {
-            pq.push({nums1[i+1]+nums2[j], {i+1,j}});
-            vis.insert({i+1,j});
+            auto topp = pq.top();
+            pq.pop();
+            
+            int i = topp.second.first;
+            int j = topp.second.second;
+            ans.push_back({nums1[i],nums2[j]});
+            k--;
+
+            //now we have 2 chances
+            if(i+1 < n && st.find({i+1,j}) == st.end())
+            {
+                pq.push({nums1[i+1]+nums2[j], {i+1, j}});
+                st.insert({i+1,j});
+            }
+
+            if(j+1 < m && st.find({i,j+1}) == st.end())
+            {
+                pq.push({nums1[i]+nums2[j+1], {i, j+1}});
+                st.insert({i,j+1});
+            }
         }
-        //(i, j+1) -> if in bounds
-        if(j+1 < m && vis.find({i,j+1})== vis.end())
-        {
-            pq.push({nums1[i]+nums2[j+1], {i,j+1}});
-            vis.insert({i,j+1});
-        }
+        return ans;
+
 
     }
-    return res;
-    }
-
 };
