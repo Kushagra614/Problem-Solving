5f312052b4bea4dcfa8b634f2ac1dc9d7dbe3bcb
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2025-09-14 09:51:10 +0530

    Time: 0 ms (100%), Space: 19.6 MB (29.43%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,30 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // Create a dummy node to serve as the starting point for the merged list
-        ListNode* dummy = new ListNode();
-        ListNode* tail = dummy; // Tail pointer to keep track of the end of the merged list
-        
-        // While both lists are non-empty, compare their values and append the smaller one to the merged list
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val <= list2->val) {
-                tail->next = list1; // Append list1 node to merged list
-                list1 = list1->next; // Move to the next node in list1
-            } else {
-                tail->next = list2; // Append list2 node to merged list
-                list2 = list2->next; // Move to the next node in list2
+        ListNode* t1 = list1; // t1 is head for list1
+        ListNode* t2 = list2; // t2 is head for list2
+
+        ListNode* dumNode = new ListNode(-1);
+        ListNode* temp = dumNode;
+
+        while(t1 != nullptr && t2!= nullptr)
+        {
+            if(t1->val < t2->val)
+            {
+                temp->next = t1;
+                temp = t1;
+                t1 = t1->next;
             }
-            tail = tail->next; // Move the tail pointer
+            else
+            {
+                temp->next = t2;
+                temp = t2;
+                t2 = t2->next;
+            }
+        } 
+
+        if(t1!= nullptr)
+        {
+            //t1 is left
+            temp ->next = t1;
+
         }
-        
-        // Append the remaining nodes of either list1 or list2 (whichever is not empty)
-        if (list1 != nullptr) {
-            tail->next = list1;
-        } else {
-            tail->next = list2;
+        else
+        {
+            temp ->next = t2;
         }
-        
-        // Return the merged list starting from the next node of dummy (since dummy itself is just a placeholder)
-        return dummy->next;
+
+        return dumNode->next;
     }
 };
