40d737ea974deb3fa8713e3b627bfa464f8af38b
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2026-03-17 20:21:16 +0530

    Time: 61 ms (34.25%), Space: 24 MB (25.31%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -10,25 +10,77 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         //brute force
-        unordered_map<ListNode*, int>mp;
-        
-        //iterate one ll
+
+        // unordered_map<ListNode*, int>mp;
+        // //iterate one ll
+        // ListNode* curr = headA;
+        // while(curr!=NULL)
+        // {
+        //     mp[curr] = 1;
+        //     curr = curr->next;
+        // }
+
+        // ListNode* purr = headB;
+        // while(purr!=NULL)
+        // {
+        //     if(mp.find(purr) != mp.end())
+        //     {
+        //         return purr;
+        //     }
+        //     purr = purr->next;
+        // }
+        // return NULL;
+
+// --------------------------------------------------------------------------------
+        //OPT APPROACH
+
+        int lenA = 0;
         ListNode* curr = headA;
         while(curr!=NULL)
         {
-            mp[curr] = 1;
             curr = curr->next;
+            lenA++;
+            
         }
+        
 
+        int lenB = 0;
         ListNode* purr = headB;
         while(purr!=NULL)
         {
-            if(mp.find(purr) != mp.end())
-            {
-                return purr;
-            }
             purr = purr->next;
+            lenB++;
         }
+        
+        ListNode* A = headA;
+        ListNode* B = headB;
+        if(lenA > lenB)
+        {
+            int diff = lenA - lenB;
+            while(diff > 0 && A!=NULL)
+            {
+                A = A->next;
+                diff--;
+            }
+        }
+        else if(lenA < lenB)
+        {
+            int diff = lenB - lenA;
+            while(diff > 0 && B!=NULL)
+            {
+                B = B->next;
+                diff--;
+            }
+        }
+        
+        //start comparing nodes
+            while(A!=NULL && B!=NULL)
+            {
+                if(A == B) return A;
+                A = A->next;
+                B = B->next;
+            }
+            
         return NULL;
 
     }
