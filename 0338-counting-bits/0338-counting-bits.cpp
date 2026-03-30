6ec399a208c9b666c19fe76652f37d0d794695a0
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2026-03-30 11:10:18 +0530

    Time: 0 ms (100%), Space: 11.4 MB (9.26%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
+    int setBits(int n)
+    {
+        int count = 0;
+        while(n)
+        {
+            n = n & (n-1);
+            count++;
+        }
+        return count;
+    }
     vector<int> countBits(int n) {
-        vector<int> ans;
-
-        for(int i = 0; i <= n; i++) {
-            int count = 0;
-            int num = i;
-
-            while(num > 0) {
-                if(num & 1) count++;
-                num = num >> 1;
-            }
-
-            ans.push_back(count);
+        //traverse form 0 to n
+        vector<int>res;
+        // for each i, calculate the no. of set bits
+        for(int i = 0; i <= n; i++)
+        {
+            int ans = setBits(i);
+             //store them in an ans array
+            res.push_back(ans);
         }
 
-        return ans;
+        return res;
     }
 };
