a4345297283bb57881f65d2b2f005ce9fc11a7cb
Kushagra Vardhan <129066926+Kushagra614@users.noreply.github.com>
2026-04-21 13:26:23 +0530

    Time: 0 ms (100%), Space: 12.8 MB (47.05%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,9 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+
+    bool dfs(TreeNode* p, TreeNode* q)
+    {
+        if(p == NULL && q == NULL) return true;
+        else if(p == NULL && q != NULL || p != NULL && q == NULL) return false;
+        else
+        {
+            if(p->val != q->val)return false;
+            else
+            {
+                return (dfs(p->left, q->left) && dfs(p->right, q->right));
+            }
+
+        }
+        return true;
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == nullptr && q == nullptr) return true;
-        if (p == nullptr || q == nullptr) return false;
-        if (p->val != q->val) return false;
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        // we wil perform pre order traversal and check if the nodes are same or not
+        bool ans = dfs(p,q);
+        if(ans == true) return true;
+        else return false;
     }
 };
